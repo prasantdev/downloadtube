e6fedb1014ee0918e5be982f149112671eaf6004
prasantdev <prasantsahoorebel@gmail.com>
2022-01-18 22:36:40 +0530

    change path to __dirname + /index.html

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());    
 app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: './' });
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/download', (req, res) => {
